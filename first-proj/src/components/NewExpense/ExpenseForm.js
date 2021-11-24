a89c133a5f35c419ed90c988b8c6aea3964576b9
AndrewSimons3 <simdrew07@gmail.com>
2021-11-24 08:05:53 -0600

    added expense filter

--- first-proj/src/components/NewExpense/ExpenseForm.js
+++ first-proj/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState('');
 	const [enteredAmount, setEnteredAmount] = useState('');
 	const [enteredDate, setEnteredDate] = useState('');
@@ -20,19 +20,37 @@ const ExpenseForm = () => {
 	const dateChangeHandler = (e) => {
 		setEnteredDate(e.target.value);
 		console.log(enteredDate);
-	};
+  };
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+  };
+
+  
 
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' onChange={titleChangeHandler}></input>
+					<input type='text' value={enteredTitle} onChange={titleChangeHandler}></input>
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
 					<input
-						type='number'
+            type='number'
+            value={enteredAmount}
 						min='0.01'
 						step='0.01'
 						onChange={amountChangeHandler}
@@ -41,7 +59,8 @@ const ExpenseForm = () => {
 				<div className='new-expense__control'>
 					<label>Date</label>
 					<input
-						type='date'
+            type='date'
+            value={enteredDate}
 						min='2019-01-01'
 						max='2022-12-31'
 						onChange={dateChangeHandler}
