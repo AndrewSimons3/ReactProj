5629a144d4939c80e5ea507821588cba5a737a9f
AndrewSimons3 <simdrew07@gmail.com>
2021-11-27 10:14:19 -0600

    modal created and implementation of clicking button or backdrop to remove modal

--- PracticeProj/src/components/UI/ErrorModal.js
+++ PracticeProj/src/components/UI/ErrorModal.js
@@ -1,20 +1,23 @@
 import Card from './Card';
-import Button from './Card';
+import Button from './Button';
 import styles from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
-	return (
-		<Card className={styles.modal}>
-			<header className={styles.header}>
-				<h2>{props.title}</h2>
-			</header>
-			<div className={styles.content}>
-				<p>{props.message}</p>
-			</div>
-			<footer className={styles.action}>
-				<Button>Okay</Button>
-			</footer>
-		</Card>
+  return (
+		<div>
+			<div className={styles.backdrop} onClick={props.onConfirm} />
+			<Card className={styles.modal}>
+				<header className={styles.header}>
+					<h2>{props.title}</h2>
+				</header>
+				<div className={styles.content}>
+					<p>{props.message}</p>
+				</div>
+				<footer className={styles.actions}>
+					<Button onClick={props.onConfirm}>Okay</Button>
+				</footer>
+			</Card>
+		</div>
 	);
 };
 
