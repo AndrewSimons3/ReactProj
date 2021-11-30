be997714de2415fdae8b7ade04ca319eed31e2ac
AndrewSimons3 <simdrew07@gmail.com>
2021-11-30 07:42:30 -0600

    created backdrop and modaloverlay components and created portal for the Cart component

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
-import styles from 'Cart.module.css';
+import styles from './Cart.module.css';
+import Modal from '../UI/Modal';
 
 const Cart = (props) => {
 	const cartItems = (
@@ -10,17 +11,17 @@ const Cart = (props) => {
 	);
 
 	return (
-		<div>
+		<Modal>
 			{cartItems}
-      <div className={styles.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={styles.actions}>
-        <button className={styles['button--alt']}></button>
-        <button className={styles.button}>Order</button>
-      </div>
-		</div>
+			<div className={styles.total}>
+				<span>Total Amount</span>
+				<span>35.62</span>
+			</div>
+			<div className={styles.actions}>
+				<button className={styles['button--alt']}>Close</button>
+				<button className={styles.button}>Order</button>
+			</div>
+		</Modal>
 	);
 };
 
