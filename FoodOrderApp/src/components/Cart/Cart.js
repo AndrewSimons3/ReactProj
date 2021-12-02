3c30f0e1e97bc6a4a449c45748b5ee659ba4fd4d
AndrewSimons3 <simdrew07@gmail.com>
2021-12-02 08:15:49 -0600

    remove items from cart

--- FoodOrderApp/src/components/Cart/Cart.js
+++ FoodOrderApp/src/components/Cart/Cart.js
@@ -11,7 +11,9 @@ const Cart = (props) => {
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
-	const cartItemRemoveHandler = (id) => {};
+	const cartItemRemoveHandler = (id) => {
+		cartCtx.removeItem(id)
+	};
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({...item, amount: 1});
@@ -26,7 +28,7 @@ const Cart = (props) => {
 					amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler(null, item)}
+          onAdd={cartItemAddHandler.bind(null, item)}
 				/>
 			))}
 		</ul>
